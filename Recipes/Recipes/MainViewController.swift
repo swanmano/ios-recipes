3928e98d4dee64ee791cd1d39cbf798c0de90b4c
Craig Swanson <cswanson02@me.com>
2019-10-30 10:14:38 -0500

    Created MainViewController and components

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Craig Swanson on 10/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: Outlets
+    @IBOutlet weak var recipeTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    @IBAction func searchRecipes(_ sender: UITextField) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
