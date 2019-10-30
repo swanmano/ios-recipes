2898e41c679800c54482200470a92b6f292ad13c
Craig Swanson <cswanson02@me.com>
2019-10-30 10:24:07 -0500

    Added Detail VC

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Craig Swanson on 10/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Outlets
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeDetailTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
